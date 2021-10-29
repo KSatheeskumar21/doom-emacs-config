6ff5396468c6fdf6fd8f6325b54a38f9ceab2509
Kishore S <k.sath214@gmail.com>
2021-10-29 16:20:59 +0100

    Initial Commit

---
+++ config.el
@@ -0,0 +1,15 @@
+(setq user-full-name "Kishore S"
+      user-mail-address "k.sath214@gmail.com")
+
+(setq doom-theme 'doom-dracula)
+
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14)
+      doom-big-font (font-spec :family "JetBrains Mono" :size 20))
+
+(setq org-directory "~/Projects/Org")
+
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;;(setq fancy-splash-image (concat doom-private-dir ""))
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
