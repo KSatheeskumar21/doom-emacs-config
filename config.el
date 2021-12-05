fe27f790c246dd6dbe1fb12456ea395978009aab
Kishore S <k.sath214@gmail.com>
2021-12-05 21:37:42 +0000

    Updates

--- config.el
+++ config.el
@@ -37,8 +37,8 @@
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t)
   (setq dashboard-banner-logo-title "I have spent way too much time procrastinating")
-  ;;(setq dashboard-startup-banner 'logo)
-  (setq dashboard-startup-banner "~/.doom.d/emacs-dash.png")
+  (setq dashboard-startup-banner 'logo)
+  ;;(setq dashboard-startup-banner "~/.doom.d/emacs-dash.png")
   (setq dashboard-center-content nil)
   (setq dashboard-recent-items '((recents . 5)
                                  (agenda . 5)
@@ -64,39 +64,39 @@
        :desc "Org Detangle" "d" #'org-babel-detangle
        :desc "Org Results" "r" #'org-babel-open-src-block-result))
 
-(setq centaur-tabs-set-bar 'over
-      centaur-tabs-set-icons t
-      centaur-tabs-gray-out-icons 'buffer
-      centaur-tabs-height 24
-      centaur-tabs-set-modified-marker t
-      centaur-tabs-style "bar"
-      centaur-tabs-modified-marker "•")
-(map! :leader
-      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
-      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
-(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
-                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
-                                               (kbd "g <down>")  'centaur-tabs-forward-group
-                                               (kbd "g <up>")    'centaur-tabs-backward-group)
+;;(setq centaur-tabs-set-bar 'over
+;;      centaur-tabs-set-icons t
+;;      centaur-tabs-gray-out-icons 'buffer
+;;      centaur-tabs-height 12
+;;      centaur-tabs-set-modified-marker t
+;;     centaur-tabs-style "bar"
+;;      centaur-tabs-modified-marker "•")
+;;(map! :leader
+;;      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
+;;      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
+;;(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
+;;                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
+;;                                               (kbd "g <down>")  'centaur-tabs-forward-group
+;;                                               (kbd "g <up>")    'centaur-tabs-backward-group)
 
-;;(setq ivy-posframe-display-functions-alist
-;;      '((swiper                     . ivy-posframe-display-at-point)
-;;        (complete-symbol            . ivy-posframe-display-at-point)
-;;        (counsel-M-x                . ivy-display-function-fallback)
-;;        (counsel-esh-history        . ivy-posframe-display-at-window-center)
-;;        (counsel-describe-function  . ivy-display-function-fallback)
-;;        (counsel-describe-variable  . ivy-display-function-fallback)
-;;        (counsel-find-file          . ivy-display-function-fallback)
-;;        (counsel-recentf            . ivy-display-function-fallback)
-;;        (counsel-register           . ivy-posframe-display-at-frame-bottom-window-center)
-;;        (nil                        . ivy-posframe-display))
-;;      ivy-posframe-height-alist
-;;      '((swiper . 20)
-;;        (t . 10)))
-;;(ivy-posframe-mode 1) ; 1 enables posframe-mode, 0 disables it.
+(setq ivy-posframe-display-functions-alist
+      '((swiper                     . ivy-posframe-display-at-point)
+        (complete-symbol            . ivy-posframe-display-at-point)
+        (counsel-M-x                . ivy-display-function-fallback)
+        (counsel-esh-history        . ivy-posframe-display-at-window-center)
+        (counsel-describe-function  . ivy-display-function-fallback)
+        (counsel-describe-variable  . ivy-display-function-fallback)
+        (counsel-find-file          . ivy-display-function-fallback)
+        (counsel-recentf            . ivy-display-function-fallback)
+        (counsel-register           . ivy-posframe-display-at-frame-bottom-window-center)
+        (nil                        . ivy-posframe-display))
+      ivy-posframe-height-alist
+      '((swiper . 20)
+        (t . 10)))
+(ivy-posframe-mode 1) ; 1 enables posframe-mode, 0 disables it.
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(helm-mode 1)
+;;(global-set-key (kbd "M-x") 'helm-M-x)
+;;(helm-mode 1)
 
 (require 'rust-mode)
 (add-hook 'rust-mode-hook #'lsp
--- init.el
+++ init.el
@@ -21,13 +21,13 @@
 
        :completion
        company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
+       ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
+       ivy               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
-       ;;deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        ;;doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
@@ -42,7 +42,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs              ; a tab bar for Emacs
+       ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
